bc1458855f08b1ac074c7bcb7e76ffd959c6e46b
Roman Seleznyov <63647042+Romashka8@users.noreply.github.com>
2025-01-05 13:13:40 +0300

    Layers
    Added main layer class and Linear Layer implementation.

--- TorchLikeFramework/my_framework.py
+++ TorchLikeFramework/my_framework.py
@@ -200,6 +200,28 @@ class SGD(object):
                 p.grad.data *= 0
 
 
+# adding base layer class
+class Layer(object):
+    def __init__(self):
+        self.parameters = list()
+
+    def get_parameters(self):
+        return self.parameters
+
+
+class Linear(Layer):
+    def __init__(self, n_inputs, n_outputs):
+        super().__init__()
+        w = np.random.randn(n_inputs, n_outputs) * np.sqrt(2.0 / n_inputs)
+        self.weight = Tensor(w, autograd=True)
+        self.bias = Tensor(np.zeros(n_outputs), autograd=True)
+
+        self.parameters.append(self.weight)
+        self.parameters.append(self.bias)
+
+    def forward(self, input):
+        return input.mm(self.weight) + self.bias.expand(0, len(input.data))
+
 if __name__ == '__main__':
     a = Tensor([1, 2, 3, 4, 5], autograd=True)
     b = Tensor([2, 2, 2, 2, 2], autograd=True)
